adab3cc8e18897a6e6e33fddc94a2cc5e0a962c4
Amauri <hamauri@aol.com>
2023-01-25 14:12:34 -0800

    user routes created & find/create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,56 @@
 const { User } = require('../models');
+const { ObjectId } = require('mongoose').Types;
 
-module.exports = {};
+// gets all user
+const getUsers = (req, res) => {
+    User.find({}, (err, result) => {
+        if (err) {
+            res.status(500).send({ err, message: 'Internal Server Error' });
+        } else {
+            res.status(200).json(result);
+        }
+    });
+};
+
+// get single user
+const getSingleUser = (req, res) => {
+    User.findOne({ _id: req.params.userId }, (err, results) =>
+        err ? res.status(500).json(err) : res.json(results)
+    );
+};
+
+// create user
+const createUser = (req, res) => {
+    const { username, email } = req.body;
+    User.create({ username, email }, (err, results) =>
+        err ? res.status(500).json(err) : res.json(results)
+    );
+}
+
+// update user
+const updateUser = (req, res) => {
+
+}
+
+// delete user
+const deleteUser = (req, res) => {
+
+}
+
+const addFriend = (req, res) => {
+    const _id = ObjectId(req.params.id);
+}
+
+const deleteFriend = (req, res) => {
+    const _id = ObjectId(req.params.id);
+}
+
+module.exports = {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+};
