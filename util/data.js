e64609c9cdc87da5bbe0744622dbde4273ba1ed5
Amauri <hamauri@aol.com>
2023-01-25 17:36:56 -0800

    updated thought data

--- util/data.js
+++ util/data.js
@@ -11,15 +11,53 @@ const userData = [
 ];
 
 const thoughtData = [
-  { thoughtText: 'wow', username: 'Aaran' },
-  { thoughtText: 'yikes', username: 'Aaren' },
-  { thoughtText: 'huh', username: 'Aarez' },
-  { thoughtText: 'yup', username: 'Aarman' },
-  { thoughtText: 'who?', username: 'Aaron' },
-  { thoughtText: 'great', username: 'Aaron-James' },
-  { thoughtText: 'awesome', username: 'Aarron' },
-  { thoughtText: 'preach!', username: 'Aaryan' },
-  { thoughtText: 'pogers', username: 'Aaryn' },
+  {
+    thoughtText:
+      'Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing.',
+    username: 'Aaran',
+  },
+  {
+    thoughtText:
+      'Up maids me an ample stood given. Certainty say suffering his him collected intention promotion. Hill sold ham men made lose case. Views abode law heard jokes too. Was are delightful solicitude discovered collecting man day. Resolving neglected sir tolerably but existence conveying for. Day his put off unaffected literature partiality inhabiting.',
+    username: 'Aaren',
+  },
+  {
+    thoughtText:
+      'Tiled say decay spoil now walls meant house. My mr interest thoughts screened of outweigh removing. Evening society musical besides inhabit ye my. Lose hill well up will he over on. Increasing sufficient everything men him admiration unpleasing sex. Around really his use uneasy longer him man. His our pulled nature elinor talked now for excuse result. Admitted add peculiar get joy doubtful.',
+    username: 'Aarez',
+  },
+  {
+    thoughtText:
+      'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy.',
+    username: 'Aarman',
+  },
+  {
+    thoughtText:
+      'Procuring education on consulted assurance in do. Is sympathize he expression mr no travelling. Preference he he at travelling in resolution. So striking at of to welcomed resolved. Northward by described up household therefore attention. Excellence decisively nay man yet impression for contrasted remarkably. There spoke happy for you are out. Fertile how old address did showing because sitting replied six. Had arose guest visit going off child she new.',
+    username: 'Aaron',
+  },
+  {
+    thoughtText:
+      'Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.',
+    username: 'Aaron-James',
+  },
+  {
+    thoughtText:
+      'Among going manor who did. Do ye is celebrated it sympathize considered. May ecstatic did surprise elegance the ignorant age. Own her miss cold last. It so numerous if he outlived disposal. How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved resolution. Hence hopes noisy may china fully and. Am it regard stairs branch thirty length afford.',
+    username: 'Aarron',
+  },
+  {
+    thoughtText:
+      'Over fact all son tell this any his. No insisted confined of weddings to returned to debating rendered. Keeps order fully so do party means young. Table nay him jokes quick. In felicity up to graceful mistaken horrible consider. Abode never think to at. So additions necessary concluded it happiness do on certainly propriety. On in green taken do offer witty of.',
+    username: 'Aaryan',
+  },
+  {
+    thoughtText:
+      'Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects.',
+    username: 'Aaryn',
+  },
 ];
 
-module.exports = { userData, thoughtData };
+const reactionData = [];
+
+module.exports = { userData, thoughtData, reactionData };
