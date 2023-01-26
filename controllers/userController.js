4a687a82339a71244dd331d2bdc31b2f08448f66
Amauri <hamauri@aol.com>
2023-01-26 12:52:39 -0800

    changed controllers to use await

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,64 +2,99 @@ const { User } = require('../models');
 const { ObjectId } = require('mongoose').Types;
 
 // gets all user
-const getUsers = (req, res) => {
-  User.find({}, '-__v', (err, results) =>
-    err ? res.status(500).json(err) : res.json(results)
-  );
+const getUsers = async (req, res) => {
+  try {
+    const results = await User.find({}).exec();
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // get single user
-const getSingleUser = (req, res) => {
-  User.findOne({ _id: req.params.userId }, '-__v', (err, results) =>
-    err ? res.status(500).json(err) : res.json(results)
-  );
+const getSingleUser = async (req, res) => {
+  try {
+    const results = await User.find({ _id: req.params.userId })
+      .populate(['friends', 'thoughts'])
+      .exec();
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // create user
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { username, email } = req.body;
-  User.create({ username, email }, (err, results) =>
-    err ? res.status(500).json(err) : res.json(results)
-  );
+
+  try {
+    const results = await User.create({ username, email });
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // update user
-const updateUser = (req, res) => {
-  User.findByIdAndUpdate(
-    req.params.userId,
-    { $set: { ...req.body } },
-    { new: true },
-    (err, results) => (err ? res.status(500).json(err) : res.json(results))
-  );
+const updateUser = async (req, res) => {
+  try {
+    const results = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $set: { ...req.body } },
+      { new: true }
+    );
+
+    results ? res.json(results) : res.json({ message: 'No user found' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
 
 // delete user
-const deleteUser = (req, res) => {
-  User.deleteOne({ _id: req.params.userId }, (err, results) =>
-    err ? res.status(500).json(err) : res.json(results)
-  );
+const deleteUser = async (req, res) => {
+  try {
+    const results = await User.deleteOne({ _id: req.params.userId });
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // add friend id to user
-const addFriend = (req, res) => {
+const addFriend = async (req, res) => {
   const { userId, friendId } = req.params;
-  User.findByIdAndUpdate(
-    userId,
-    { $addToSet: { friends: friendId } },
-    { new: true },
-    (err, results) => (err ? res.status(500).json(err) : res.json(results))
-  );
+  try {
+    const results = await User.findByIdAndUpdate(
+      userId,
+      { $addToSet: { friends: friendId } },
+      { new: true }
+    );
+
+    results ? res.json(results) : res.json({ message: 'No user found' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // delete friend id from user
-const deleteFriend = (req, res) => {
+const deleteFriend = async (req, res) => {
   const { userId, friendId } = req.params;
-  User.findByIdAndUpdate(
-    userId,
-    { $pull: { friends: friendId } },
-    { new: true },
-    (err, results) => (err ? res.status(500).json(err) : res.json(results))
-  );
+  try {
+    const results = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { friends: friendId } },
+      { new: true }
+    );
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
